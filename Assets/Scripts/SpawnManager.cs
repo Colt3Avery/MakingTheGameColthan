b1cd3e1b9ff79399f4018429ab7dee35e10201df
Colthan <avecolth@gmail.com>
2020-03-23 11:51:45 -0700

    pushAfterLesson
    Haven't finalized

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnLocation = new Vector3(30, 0, 0);
+    private float initialWait = 2;
+    private float peroidRepeat = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", initialWait, peroidRepeat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
+    }
+}
